2c0ee56d88b49bb409e18002dbb434df7154f401
ggahlotInformatica <ggahlot@informatica.com>
2019-11-07 09:06:10 +0530

    Longest Decresing Subsequence

--- LongestDecreasingSubsequence.java
+++ LongestDecreasingSubsequence.java
@@ -1,6 +1,6 @@
-import java.util.Stack;
+import java.util.ArrayList;
 
-public class LongestIncreasingSubsequence {
+public class LongestDecreasingSubsequence {
     static class Result {
         int[] dummy;
         int maxSoFar;
@@ -12,29 +12,29 @@ public class LongestIncreasingSubsequence {
     }
 
     public static void main(String args[]) {
-        int a[] = {10, 22, 9, 33, 21, 50, 41, 60, 80};
-//        int a[] = {1, 11, 2, 10, 4, 5, 2, 1};
-        int b[] = printLIS(a);
+//        int a[] = {10, 22, 9, 33, 21, 50, 41, 60, 80};
+        int a[] = {1, 11, 2, 10, 4, 5, 2, 1};
+        int b[] = printLDS(a);
         for (int i = 0; i < b.length; i++) {
             System.out.println(b[i]);
         }
     }
 
-    static int[] printLIS(int a[]) {
-        Result res = getLIS(a);
+    static int[] printLDS(int a[]) {
+        Result res = getLDS(a);
         int dummy[] = res.dummy;
         int maxSoFar = res.maxSoFar;
-        return getLISHelper(a, dummy, maxSoFar);
+        return getLDSHelper(a, dummy, maxSoFar);
     }
 
-    static Result getLIS(int a[]) {
+    static Result getLDS(int a[]) {
         int dummy[] = new int[a.length];
         int maxSoFar = 1;
         for (int i = 0; i < dummy.length; i++) {
             dummy[i] = 1;
         }
-        for (int i = 0; i < a.length - 1; i++) {
-            for (int j = i + 1; j < a.length; j++) {
+        for (int i = a.length - 1; i > 0; i--) {
+            for (int j = i - 1; j >= 0; j--) {
                 if (a[j] > a[i]) {
                     if (dummy[j] < dummy[i] + 1) {
                         dummy[j] = dummy[j] + 1;
@@ -46,25 +46,25 @@ public class LongestIncreasingSubsequence {
         return new Result(dummy, maxSoFar);
     }
 
-    static int[] getLISHelper(int a[], int dummy[], int maxSoFar) {
-        Stack<Integer> stack = new Stack<Integer>();
-        int i = dummy.length - 1;
+    static int[] getLDSHelper(int a[], int dummy[], int maxSoFar) {
+        ArrayList<Integer> queue = new ArrayList<Integer>();
+        int i = 0;
         int toPush = maxSoFar;
-        while (i > 0) {
-            while (i > 0 && dummy[i] != toPush) {
-                i--;
+        while (i < dummy.length - 1) {
+            while (i < dummy.length - 1 && (dummy[i] != toPush || dummy[i] == dummy[i + 1])) {
+                i++;
             }
-            if (i > 0) {
-                stack.push(a[i]);
+            if (i < dummy.length - 1) {
+                queue.add(a[i]);
                 toPush = toPush - 1;
-                i--;
+                i++;
             } else break;
         }
-        if (dummy[0] == toPush) stack.push(a[0]);
-        int toReturn[] = new int[stack.size()];
+        if (dummy[dummy.length - 1] == toPush) queue.add(a[dummy.length - 1]);
+        int toReturn[] = new int[queue.size()];
         i = 0;
-        while (!stack.empty()) {
-            toReturn[i++] = stack.pop();
+        for (int ele : queue) {
+            toReturn[i++] = ele;
         }
         return toReturn;
     }
